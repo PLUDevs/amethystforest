a1dabf4dc416aed6eec262f2f6f984c8c118cddf
Patrick Opie <opiepj@plu.edu>
2014-09-28 18:06:53 -0700

    (Source files)

---
+++ amethystforest/Source/amethystforest/amethystforestGameMode.h
@@ -0,0 +1,18 @@
+
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "amethystforestGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AMETHYSTFOREST_API AamethystforestGameMode : public AGameMode
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
---
+++ amethystforest/Source/amethystforest/amethystforestPlayerController.cpp
@@ -0,0 +1,13 @@
+
+
+#include "amethystforest.h"
+#include "amethystforestPlayerController.h"
+
+
+AamethystforestPlayerController::AamethystforestPlayerController(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+
+
