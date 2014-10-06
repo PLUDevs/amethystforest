20884f9b4f58a2041a8e60e522aec01ad515ceb9
spitfire21 <cody_bot@hotmail.com>
2014-10-05 20:33:52 -0700

    Base AI class has some features removed will be added later once gamemode and player states are available. AmethystBot is example character that is used with AI

---
+++ amethystforest/Source/amethystforest/AmethystBot.h
@@ -0,0 +1,47 @@
+
+
+#pragma once
+
+#include "AmethystCharacter.h"
+#include "AmethystBot.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AMETHYSTFOREST_API AAmethystBot : public AAmethystCharacter
+{
+	GENERATED_UCLASS_BODY()
+
+
+		UPROPERTY(EditDefaultsOnly, Category = Behaviour)
+		float AttackRange;
+	UPROPERTY(EditDefaultsOnly, Category = Behaviour)
+		float AttackDamage;
+
+	UPROPERTY(EditAnywhere, Category = Behavior)
+	class UBehaviorTree* BotBehavior;
+
+	UFUNCTION(BlueprintCallable, Category = Behavior)
+		void PerformMeleeAttack();
+
+	float AccumulatedFiretime;
+	virtual void TakeDamage();
+	virtual bool IsFirstPerson() const OVERRIDE;
+
+
+	virtual void ReceiveHit
+		(
+	class UPrimitiveComponent * MyComp,
+	class AActor * Other,
+	class UPrimitiveComponent * OtherComp,
+		bool bSelfMoved,
+		FVector HitLocation,
+		FVector HitNormal,
+		FVector NormalImpulse,
+		const FHitResult & Hit
+		) OVERRIDE;
+
+	virtual void FaceRotation(FRotator NewRotation, float DeltaTime = 0.f) OVERRIDE;
+	bool Attacking;
+};
