719469dcf23db645efaab6dbf03df4228e3b24f0
Patrick Opie <patrick@Patricks-MacBook-Pro.local>
2014-11-19 22:51:05 -0800

    Removed Intermediate folder

--- amethystforest/Intermediate/Build/Win64/amethystforestEditor/DebugGame/amethystforest/amethystforest.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_AMETHYSTFOREST_H__
-#define __AUTO_AMETHYSTFOREST_H__
-//Last Write: 10/2/2014 3:08:53 AM +00:00
-#include "E:\Pat's Documents\GitHub\amethystforest\amethystforest\Source\amethystforest\amethystforest.h"
-#endif//__AUTO_AMETHYSTFOREST_H__
--- amethystforest/Intermediate/Build/Win64/amethystforestEditor/Development/amethystforest/amethystforest.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_AMETHYSTFOREST_H__
-#define __AUTO_AMETHYSTFOREST_H__
-//Last Write: 10/8/2014 3:44:25 AM +00:00
-#include "E:\Pat's Documents\GitHub\amethystforest\amethystforest\Source\amethystforest\Public\amethystforest.h"
-#endif//__AUTO_AMETHYSTFOREST_H__
