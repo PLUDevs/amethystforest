6b2192d81a6fa7bf2668a7418388b6868bebeaa0
Patrick Opie <opiepj@plu.edu>
2015-01-21 20:41:45 -0800

    (#37) Engine class, doesnt work. Planning to remove.

---
+++ amethystforest/Source/amethystforest/Classes/AmethystEngine.h
@@ -0,0 +1,15 @@
+
+//#include "AmethystEngine.generated.h"
+
+//UCLASS()
+//TO DO: Figure this out
+//class AmethystGAME_API UAmethystEngine : public UGameEngine
+//{
+//    GENERATED_UCLASS_BODY()
+//    
+//    /* Hook up specific callbacks */
+//    virtual void Init(IEngineLoop* InEngineLoop);
+//    
+//};
+
+
---
+++ amethystforest/Source/amethystforest/Private/AmethystEngine.cpp
@@ -0,0 +1,15 @@
+
+#include "amethystforest.h"
+//TO DO: Figure out why this doesn't work'
+//UAmethystEngine::UAmethystEngine(const class FPostConstructInitializeProperties& PCIP)
+//: Super(PCIP)
+//{
+//}
+//
+//void UAmethystEngine::Init(IEngineLoop* InEngineLoop)
+//{
+//    // Note: Lots of important things happen in Super::Init(), including spawning the player pawn in-game and
+//    // creating the renderer.
+//    Super::Init(InEngineLoop);
+//}
+
