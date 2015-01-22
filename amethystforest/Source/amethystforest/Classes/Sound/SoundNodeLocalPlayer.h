ed88861c26d7a4d4126edbae2dd5e405637770fd
Patrick Opie <opiepj@plu.edu>
2015-01-22 14:18:12 -0800

    (#42) SoundNode Class, GetUniqueString was removed in 4.5

---
+++ amethystforest/Source/amethystforest/Classes/Sound/SoundNodeLocalPlayer.h
@@ -0,0 +1,25 @@
+
+#include "Sound/SoundNode.h"
+#include "SoundNodeLocalPlayer.generated.h"
+
+/**
+ * Choose different branch for sounds attached to locally controlled player
+ */
+UCLASS(hidecategories=Object, editinlinenew)
+class USoundNodeLocalPlayer : public USoundNode
+{
+    GENERATED_UCLASS_BODY()
+    
+    // Begin USoundNode interface.
+    virtual void ParseNodes( FAudioDevice* AudioDevice, const UPTRINT NodeWaveInstanceHash, FActiveSound& ActiveSound, const FSoundParseParameters& ParseParams, TArray<FWaveInstance*>& WaveInstances ) override;
+    virtual void CreateStartingConnectors( void ) override;
+    virtual int32 GetMaxChildNodes() const override;
+    virtual int32 GetMinChildNodes() const override;
+    // WARNING: No GetUniqueString()
+    //virtual FString GetUniqueString() const override;
+     
+#if WITH_EDITOR
+    virtual FString GetInputPinName(int32 PinIndex) const override;
+#endif
+    // End USoundNode interface.
+};
