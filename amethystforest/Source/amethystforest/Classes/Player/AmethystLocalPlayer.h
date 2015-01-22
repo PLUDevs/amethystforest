f48a1068b2f8f09be1bb39940316b7aed556fc6a
Patrick Opie <opiepj@plu.edu>
2015-01-22 12:44:45 -0800

    (#35) Camera Manager, Persistent and local player classes.

---
+++ amethystforest/Source/amethystforest/Classes/Player/AmethystLocalPlayer.h
@@ -0,0 +1,27 @@
+
+#include "AmethystPersistentUser.h"
+#include "AmethystLocalPlayer.generated.h"
+
+UCLASS(config=Engine, transient)
+class UAmethystLocalPlayer : public ULocalPlayer
+{
+    GENERATED_UCLASS_BODY()
+    
+public:
+    
+    virtual void SetControllerId(int32 NewControllerId) override;
+    
+    class UAmethystPersistentUser* GetPersistentUser() const;
+    
+    /** Initializes the PersistentUser */
+    void LoadPersistentUser();
+    
+private:
+    /** Persistent user data stored between sessions (i.e. the user's savegame) */
+    UPROPERTY()
+    class UAmethystPersistentUser* PersistentUser;
+    
+};
+
+
+
